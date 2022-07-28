c09a9cbb07349217e1d33bb4ce58375519732496
Mathaus Annibal <mathausannibal@gmail.com>
2022-07-28 20:00:04 -0300

    sobe desafio raspagem de dados em python

---
+++ tech_news/analyzer/search_engine.py
@@ -0,0 +1,54 @@
+from tech_news.database import search_news
+from datetime import datetime
+
+
+# Requisito 6
+def search_by_title(title):
+    """Seu código deve vir aqui"""
+    data_base = search_news({"title": {"$regex": title, "$options": "i"}})
+    noticias = []
+
+    for noticia in data_base:
+        news = (noticia["title"], noticia["url"])
+        noticias.append(news)
+    return noticias
+
+
+# Requisito 7
+def search_by_date(date):
+    """Seu código deve vir aqui"""
+    noticia = []
+    try:
+        datetime.strptime(date, "%Y-%m-%d")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    data_base = search_news({"timestamp": {"$regex": date, "$options": "i"}})
+    for data in data_base:
+        new = (data["title"], data["url"])
+        noticia.append(new)
+    return noticia
+
+
+# Requisito 8
+def search_by_source(source):
+    """Seu código deve vir aqui"""
+    noticia = []
+    data_base = search_news({"sources": {"$regex": source, "$options": "i"}})
+    for data in data_base:
+        source = (data["title"], data["url"])
+        noticia.append(source)
+    return noticia
+
+
+# Requisito 9
+def search_by_category(category):
+    """Seu código deve vir aqui"""
+    noticia = []
+    data_base = search_news(
+        {"categories": {"$regex": category, "$options": "i"}}
+    )
+    for data in data_base:
+        categoria = (data["title"], data["url"])
+        noticia.append(categoria)
+    return noticia
---
+++ tech_news/scraper.py
@@ -0,0 +1,124 @@
+import requests
+import time
+from parsel import Selector
+from tech_news.database import create_news
+
+
+# Requisito 1
+def fetch(url):
+    """Seu código deve vir aqui"""
+    try:
+        # recurso demora muito a responder
+        response = requests.get(url, timeout=3)
+        time.sleep(1)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.ReadTimeout:
+        # vamos fazer uma nova requisição
+        return None
+
+
+# Requisito 2
+def scrape_novidades(html_content):
+    """Seu código deve vir aqui"""
+    list = []
+    selector = Selector(text=html_content)
+
+    for url in selector.css(
+        ".tec--list__item .tec--card__title__link::attr(href)"
+    ).getall():
+        list.append(url)
+
+    return list
+
+
+# Requisito 3
+def scrape_next_page_link(html_content):
+    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_url = selector.css("div.tec--list a.tec--btn::attr(href)").getall()
+    if next_url:
+        return next_url[0]
+    else:
+        return None
+
+
+# Requisito 4
+def scrape_noticia(html_content):
+    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    url = selector.css('meta[property="og:url"]::attr(content)').get()
+
+    title = selector.css("h1#js-article-title::text").get()
+
+    timestamp = selector.css(
+        ".tec--timestamp__item time::attr(datetime)"
+    ).get()
+
+    writer = selector.css(".z--font-bold ::text").get()
+    if writer:
+        writer = writer.strip()
+    else:
+        writer = None
+
+    shares = selector.css(".tec--toolbar__item::text").get()
+    if shares:
+        shares_count = shares.split()[0]
+    else:
+        shares_count = 0
+
+    comments_count = selector.css("#js-comments-btn::attr(data-count)").get()
+    summary = "".join(
+        selector.css(".tec--article__body > p:first_child *::text").getall()
+    ).strip()
+
+    sources = []
+    source = selector.css(".z--mb-16 .tec--badge::text").getall()
+    for s in source:
+        sources.append(s.strip())
+
+    categories = []
+    category = selector.css("#js-categories a::text").getall()
+    for categorie in category:
+        categories.append(categorie.strip())
+
+    data = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "shares_count": int(shares_count),
+        "comments_count": int(comments_count),
+        "summary": summary,
+        "sources": sources,
+        "categories": categories,
+    }
+    return data
+
+
+# Requisito 5
+def get_tech_news(amount):
+    """Seu código deve vir aqui"""
+    url = "https://www.tecmundo.com.br/novidades"
+
+    link = fetch(url)
+    news_urls = scrape_novidades(link)
+    noticias = []
+
+    while len(news_urls) < amount:
+
+        url = scrape_next_page_link(link)
+        link = fetch(url)
+        add_url = scrape_novidades(link)
+        news_urls.extend(add_url)
+    # teste subindo o arquivo de novo
+    for new_url in news_urls:
+        if len(noticias) < amount:
+            new_content = fetch(new_url)
+            scraped = scrape_noticia(new_content)
+            noticias.append(scraped)
+
+    create_news(noticias)
+    return noticias
